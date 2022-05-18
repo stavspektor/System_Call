6e0b0e5d5e5073a95bb9a9e5c7e5db711151c35a
Stav Spektor <96288050+stavspektor@users.noreply.github.com>
2022-05-18 11:54:06 +0300

    Add files via upload

---
+++ lsproc.c
@@ -0,0 +1,19 @@
+// user space program to call the syscall
+
+// TODO: you have to fill in the missing includes statements and code
+// to make the code usable. 
+// Hint: look at other user-space programs in this directory
+//
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main()
+{
+  lsproc();
+  
+  exit();
+}
